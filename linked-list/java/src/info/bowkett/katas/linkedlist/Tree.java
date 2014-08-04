1726fa38a228d3c77fcaa689294b2e7032d817f3
jbowkett <james_bowkett@lyagushka.co.uk>
2014-08-04 15:12:14 +0000

    Merge of the feature branch into the topic branch

---
+++ linked-list/java/src/info/bowkett/katas/linkedlist/Tree.java
@@ -0,0 +1,11 @@
+package info.bowkett.katas.linkedlist;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jbowkett
+ * Date: Aug 12, 2013
+ * Time: 5:47:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Tree {
+}
