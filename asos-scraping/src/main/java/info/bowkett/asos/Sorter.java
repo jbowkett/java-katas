b67f62690d6b6289f0fa331d4defac493dbb43ad
jbowkett <james@bowkett.info>
2016-04-21 22:29:31 +0000

    DRY out sorter

--- asos-scraping/src/main/java/info/bowkett/asos/Sorter.java
+++ asos-scraping/src/main/java/info/bowkett/asos/Sorter.java
@@ -15,10 +15,13 @@ public class Sorter {
   }
 
   private int compare(Offer o1, Offer o2) {
-    final double discount = o1.rrp - o1.salePrice;
-    final double percentage = discount / o1.rrp;
-    final double discount2 = o2.rrp - o2.salePrice;
-    final double percentage2 = discount2 / o2.rrp;
+    final double percentage = getDiscountPercentge(o1);
+    final double percentage2 = getDiscountPercentge(o2);
     return Double.compare(percentage2,  percentage);
+  }
+
+  private double getDiscountPercentge(Offer offer) {
+    final double discount = offer.rrp - offer.salePrice;
+    return discount / offer.rrp;
   }
 }
