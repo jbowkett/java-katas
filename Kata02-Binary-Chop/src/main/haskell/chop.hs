be3d11897c7697be049bca61db2e38fdd2859ad1
jbowkett <james@bowkett.info>
2016-06-10 16:26:29 +0000

    Updated to use pattern matching

--- Kata02-Binary-Chop/src/main/haskell/chop.hs
+++ Kata02-Binary-Chop/src/main/haskell/chop.hs
@@ -5,7 +5,7 @@ chop(lst, toFind) =
   else
     if length(lst) == 1
       then
-        error "{toFind} Not found in list."
+        error "Not found in list."
     else
       if halfwayValue < toFind
       then
@@ -19,3 +19,20 @@ chop(lst, toFind) =
   firstHalfOfList  = take halfwayIndex lst
   chopLatterHalf = chop(latterHalfOfList, toFind)
   chopFirstHalf = chop(firstHalfOfList, toFind)
+
+
+
+chop2 :: (Ord a) => ([a], a) -> Int
+chop2 (lst, x)
+   | halfwayValue == x = halfwayIndex
+   | listLength   == 1 = error "Not found in list."
+   | halfwayValue  < x = halfwayIndex + indexInLaterHalf
+   | otherwise         = indexInFirstHalf
+  where
+  listLength   = length(lst)
+  halfwayIndex = listLength `div` 2
+  halfwayValue = lst!! halfwayIndex
+  latterHalfOfList = drop halfwayIndex lst
+  firstHalfOfList  = take halfwayIndex lst
+  indexInLaterHalf = chop2(latterHalfOfList, x)
+  indexInFirstHalf = chop2(firstHalfOfList, x)
